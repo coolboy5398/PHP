c4b868576f009f3eafe53e380a095a1cf6aaa152
dzh <ts_dongzheng@163.com>
2017-01-20 16:19:17 +0800

    By Coresdy

--- PHPTest/test.php
+++ PHPTest/test.php
@@ -5,4 +5,11 @@
  * Date: 2017/1/20
  * Time: 15:08
  */
-echo "hhh";
+echo "hhh<br>";
+function add($str1,$str2){
+    $str3=$str1+$str2;
+    return $str3;
+}
+echo add(1,2)."<br>";
+$aaa=&add;
+echo $aaa(2,3);
