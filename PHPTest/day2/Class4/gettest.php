bf7a44a8346d31734a44f2acfb9b1fe038bd07f7
dzh <ts_dongzheng@163.com>
2017-03-03 15:07:33 +0800

    By Coresdy

--- PHPTest/day2/Class4/gettest.php
+++ PHPTest/day2/Class4/gettest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: dongzheng
+ * Date: 2017/3/2
+ * Time: 下午11:44
+ */
 $v1=$_GET["uName"];
 $v2=$_GET["uPswd"];
 echo "v1=$v1,v2=$v2";
@@ -7,9 +13,3 @@ echo "<pre>";
 var_dump($_GET); //var_dump 用于输出一个变量最完整的信息，包括变量名，长度，数据值
 //这里是输出"数组"$_GET
 echo "</pre>";
-/**
- * Created by PhpStorm.
- * User: dongzheng
- * Date: 2017/3/2
- * Time: 下午11:44
- */
--- PHPTest/day2/Class5/postphp.php
+++ PHPTest/day2/Class5/postphp.php
@@ -5,12 +5,20 @@
  * Date: 2017/3/3
  * Time: 12:39
  */
-$n1=$_POST["n1"];
+var_dump($_POST);
+
+/*$n1=$_POST["n1"];
 $n2=$_POST["n2"];
 $fuhao=$_POST["yunsuanfu"];
 switch ($fuhao)
 {
     case "+":
         $result=$n1+$n2;
+    case "-":
+        $result=$n1-$n2;
+    case "*":
+        $result=$n1*$n2;
+    case "、":
+        $result=$n1/$n2;
 }
-echo "$n1+$n2=$n3";
+echo "$n1+$n2=$result";*/
