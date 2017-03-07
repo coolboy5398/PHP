36306be43b6d6c62ae18b69dd2de111f54da0b86
dongzheng.mac <ts_dongzheng@163.com>
2017-03-07 22:56:30 +0800

    r

---
+++ PHPTest/day2/homework.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dongzheng
+ * Date: 2017/3/7
+ * Time: 下午10:48
+ */
+$arrname=array("REMOTE_ADDR","SERVER_ADDR","SERVER_NAME","DOCUMENT_ROOT","PHP_SELF");
+$arrdescirbe=array("用户的IP地址","服务器端的IP地址","服务器名","站点的绝对路径","网页的文件路径");
