fe5bd367bcc2067a1f3b7f8af61f8e552ac96e1b
dzh <ts_dongzheng@163.com>
2017-03-07 12:39:23 +0800

    3

---
+++ PHPTest/day2/Day2Test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/7
+ * Time: 12:33
+ */
+echo __FILE__."<br/>"; //文件绝对路径
+echo $_SERVER["PHP_SELF"]."<br/>";  //文件相对路径
+echo __DIR__."<br/>";  //文件所在路径
+echo $_SERVER["DOCUMENT_ROOT"]."<br/>";  //相对路径
